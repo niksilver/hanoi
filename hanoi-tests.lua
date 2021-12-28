dbeb94a2b3a6dc718dc7965cd3d430cae1993421
Nik Silver <nik@niksilver.com>
2021-12-28 17:54:04 +0000

    Started to add some proper string layout functions.

--- hanoi-tests.lua
+++ hanoi-tests.lua
@@ -1,12 +1,46 @@
 -- Example unit tests
 
 lu = require('luaunit')
+hanoi = require('hanoi')
 
 TestOfAdding = {} -- class
 
 function TestOfAdding:testWithZero()
     lu.assertEquals(0+3, 3, "zero and something")
     lu.assertEquals(2+0, 2, "something and zero")
+end
+
+TestUtils = {} -- class
+
+function TestUtils:testPad()
+    lu.assertEquals( hanoi.pad("Hello", 4), "Hello")
+    lu.assertEquals( hanoi.pad("Hello", 5), "Hello")
+    lu.assertEquals( hanoi.pad("Hello", 6), "Hello ")
+    lu.assertEquals( hanoi.pad("Hello", 7), "Hello  ")
+
+    lu.assertEquals( hanoi.pad(nil, 3), "   ")
+end
+
+function TestUtils:testPrefix()
+    local one = {"just one"}
+    local two = {"apples", "pears"}
+    local three = {"xxx", "yyy", "z"}
+
+    lu.assertEquals( hanoi.prefix(one, two),
+        { "just oneapples",
+          "        pears"
+        }
+    )
+    lu.assertEquals( hanoi.prefix(two, one),
+        { "applesjust one"
+        }
+    )
+    lu.assertEquals( hanoi.prefix(three, three),
+        { "xxxxxx",
+          "yyyyyy",
+          "z  z"
+        }
+    )
 end
 
 os.exit( lu.LuaUnit.run() )
